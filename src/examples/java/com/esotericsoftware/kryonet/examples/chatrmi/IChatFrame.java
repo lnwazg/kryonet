a6b9ea27903b9df36bb3a5751fefdb8572c60e4a
lnwazg <lnwazg@126.com>
2018-09-21 22:04:37 +0800

    初始化提交

---
+++ src/examples/java/com/esotericsoftware/kryonet/examples/chatrmi/IChatFrame.java
@@ -0,0 +1,10 @@
+
+package com.esotericsoftware.kryonet.examples.chatrmi;
+
+// This class represents the chat window on the client.
+public interface IChatFrame
+{
+    public void addMessage(String message);
+    
+    public void setNames(String[] names);
+}
---
+++ src/examples/java/com/esotericsoftware/kryonet/examples/position/Character.java
@@ -0,0 +1,11 @@
+
+package com.esotericsoftware.kryonet.examples.position;
+
+public class Character
+{
+    public String name;
+    
+    public String otherStuff;
+    
+    public int id, x, y;
+}
